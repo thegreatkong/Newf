958c88cbf827407cf78702048c3c233f0c1da82e
rajsinghfan <128118921+rajsinghfan@users.noreply.github.com>
2024-03-11 19:51:27 +0530

    Update getviews.py

--- getviews.py
+++ getviews.py
@@ -144,7 +144,7 @@ def tui():
 clear_output()
 print(logo)
 channel = "the_hacking_zone"
-post = "1112"
+post = "1113"
 
 try:
     search('<span class="tgme_widget_message_views">([^<]+)', requests.get(f'https://t.me/{channel}/{post}',
